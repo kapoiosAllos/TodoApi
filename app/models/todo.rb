7066aac9149208be0fa3f2a97050d1aeb15438ef
kapoiosAllos <kyriakos.z@yahoo.com>
2017-11-16 16:43:35 +0100

    End day 4

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,3 @@
-class Todo < ApplicationRecord
+class Todo < ActiveRecord::Base
+  validates :title,  presence: true
 end
--- app/views/todos/todos.js
+++ app/views/todos/todos.js
@@ -1,11 +1,10 @@
-
-
 function toggleDone() {
   var checkbox = this;
   var tableRow = $(this).parent().parent();
 
   var todoId = tableRow.data('id');
   var isCompleted = !tableRow.hasClass("success");
+
 
   $.ajax({
     type: "PUT",
@@ -14,15 +13,15 @@ function toggleDone() {
       todo: { completed: isCompleted }
     }),
     contentType: "application/json",
-    dataType: "json"
-  })
-  .done(function(data) {
-    console.log(data);
+    dataType: "json"})
 
-    tableRow.toggleClass("success", data.completed);
+    .done(function(data) {
+      console.log(data);
 
-    updateCounters();
-  });
+      tableRow.toggleClass("success", data.completed);
+
+      updateCounters();
+    });
 }
 
 function updateCounters() {
@@ -30,8 +29,6 @@ function updateCounters() {
   $("#completed-count").html($(".success").size());
   $("#todo-count").html($(".todo").size() - $(".success").size());
 }
-
-
 
 function createTodo(title) {
   var newTodo = { title: title, completed: false };
@@ -72,52 +69,38 @@ function createTodo(title) {
     console.log(error)
     error_message = error.responseJSON.title[0];
     showError(error_message);
-  });
+  })
+
+  function showError(message) {
+    var errorHelpBlock = $('<span class="help-block"></span>')
+      .attr('id', 'error_message')
+      .text(message);
+
+    $("#formgroup-title")
+      .addClass("has-error")
+      .append(errorHelpBlock);
+  }
 }
+
 
 function resetErrors() {
   $("#error_message").remove();
   $("#formgroup-title").removeClass("has-error");
 }
 
-function showError(message) {
-  var errorHelpBlock = $('<span class="help-block"></span>')
-    .attr('id', 'error_message')
-    .text(message);
-
-  $("#formgroup-title")
-    .addClass("has-error")
-    .append(errorHelpBlock);
-}
-
 function submitTodo(event) {
   event.preventDefault();
-  createTodo($("#todo_title").val());
   resetErrors();
+
+  createTodo($("#todo_title").val());
   $("#todo_title").val(null);
   updateCounters();
 }
 
 function cleanUpDoneTodos(event) {
   event.preventDefault();
-
-  $.each($(".success"), function(index, tableRow) {
-    todoId = $(tableRow).data('id');
-    deleteTodo(todoId);
-  });
-}
-
-function deleteTodo(todoId) {
-  $.ajax({
-    type: "DELETE",
-    url: "/todos/" + todoId + ".json",
-    contentType: "application/json",
-    dataType: "json"
-  })
-  .done(function(data) {
-    $('tr[data-id="'+todoId+'"]').remove();
-    updateCounters();
-  });
+  $.when($(".success").remove())
+    .then(updateCounters);
 }
 
 $(document).ready(function() {
